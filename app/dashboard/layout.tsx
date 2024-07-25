01c2f79d899d20297d77dda4596a99305198a26d
Maxime <maxime.celeste1221@gmail.com>
2024-07-25 15:52:19 +0400

    set up premium pass

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,35 @@
-import ButtonSignOut from "../components/ButtonSignOut";
-import DashboardNav from "../components/DashboardNav";
+import { getUser } from "@/lib/actionsUsers";
+import { stripe } from "@/lib/stripe";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ButtonSignOut from "../components/ButtonSignOut";
+import DashboardNav from "../components/DashboardNav";
+import { prisma } from "@/lib/db";
 
 export default async function DashboardLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const user = await getUser();
+
+  if (!user) {
+    return <div>Veuillez vous connectez</div>;
+  }
+
+  if (!user?.stripeCustomerId) {
+    const stripeCustomer = await stripe.customers.create({
+      email: user?.email as string,
+    });
+
+    await prisma.user.update({
+      where: {
+        id: user?.id as string,
+      },
+      data: {
+        stripeCustomerId: stripeCustomer.id as string,
+      },
+    });
+  }
+
   return (
     <section className="max-w-[1200px] mx-auto md:flex md:items-center md:gap-4 h-screen w-full mt-2 p-2">
       <DashboardNav />
--- app/dashboard/payment/page.tsx
+++ app/dashboard/payment/page.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { getUser } from "@/lib/actionsUsers";
+import Image from "next/image";
+import PremiumBadge from "../src/icons/PremiumBadge.svg";
 
 export default function PagePayment() {
-  return <div>PagePayment</div>;
+  const itemsPremium = [
+    { name: "Notes illimitées" },
+    { name: "Intégration de l'intelligence artificielle" },
+    { name: "Support Technique et Mises à jour" },
+  ];
+
+  return (
+    <div className="max-w-lg mx-auto space-y-4 mt-3">
+      <Card className="flex flex-col">
+        <CardContent className="py-8">
+          <div>
+            <h3 className="text-md font-black uppercase bg-purple-800 bg-opacity-20 text-purple-500 p-3 rounded-md inline">
+              Pass Premium
+            </h3>
+          </div>
+          <div className="mt-4 text-6xl font-black">
+            <span>19,99 €</span>
+            <span className="text-sm text-muted-foreground">/ par mois</span>
+          </div>
+          <p className="mt-4 text-muted-foreground">
+            Débloquer un nouveau niveau de productivité personnelle 💥
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
