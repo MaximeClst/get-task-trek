8832071443eab6dce9b7a27b07f8b2345e41c88e
Maxime <maxime.celeste1221@gmail.com>
2024-08-19 09:02:11 +0400

    edit dashboard

--- app/dashboard/payment/page.tsx
+++ app/dashboard/payment/page.tsx
@@ -1,4 +1,3 @@
-import PremiumBadge from "@/app/src/icons/PremiumBadge.svg";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import {
@@ -7,7 +6,6 @@ import {
   getDataStripeUser,
 } from "@/lib/actionsStripe";
 import { getUser } from "@/lib/actionsUsers";
-import Image from "next/image";
 
 export default async function PagePayment() {
   const user = await getUser();
@@ -34,13 +32,7 @@ export default async function PagePayment() {
               <p className="mt-4 text-sm text-muted-foreground">
                 Modifier votre abonnement premium
               </p>
-              <Image
-                src={PremiumBadge}
-                width={16}
-                height={16}
-                alt="badge"
-                className="block my-4"
-              />
+
               <form className="w-full mt-4" action={createCustomerPortal}>
                 <Button className="bg-gradient-to-r from-fuchsia-500 to-cyan-500">
                   Modifier abonnement
@@ -60,13 +52,38 @@ export default async function PagePayment() {
           <div>
             <h3 className="text-md font-black uppercase bg-purple-800 bg-opacity-20 text-purple-500 p-3 rounded-md inline">
               Pass Premium
-              <Image
-                src={PremiumBadge}
-                width={100}
-                height={100}
-                alt="badge"
-                className="block my-4"
-              />
+            </h3>
+          </div>
+          <div className="mt-4 text-6xl font-black">
+            <span>19,99 €</span>
+            <span className="text-sm text-muted-foreground">/ par mois</span>
+          </div>
+          <p className="mt-4 text-muted-foreground">
+            Débloquer un nouveau niveau de productivité personnelle 💥
+          </p>
+          <div className="flex-1 flex flex-col justify-between px-6 py-4 bg-secondary rounded-lg m-1 space-t-6 p-3 mt-4">
+            <ul className="space-y-3">
+              {itemsPremium.map((item, index) => (
+                <li key={index} className="flex items-center gap-2">
+                  <span>✅</span>
+                  <span>{item.name}</span>
+                </li>
+              ))}
+            </ul>
+            <form action={createSubscription} className="w-full mt-4">
+              <Button className="bg-gradient-to-r from-fuchsia-500 to-cyan-500 text-white">
+                Devenir membre Premium
+              </Button>
+            </form>
+          </div>
+        </CardContent>
+      </Card>
+
+      <Card className="flex flex-col">
+        <CardContent className="py-8">
+          <div>
+            <h3 className="text-md font-black uppercase bg-purple-800 bg-opacity-20 text-purple-500 p-3 rounded-md inline">
+              Pass Premium
             </h3>
           </div>
           <div className="mt-4 text-6xl font-black">
